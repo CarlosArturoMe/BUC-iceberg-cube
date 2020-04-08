3e232fbc4fb8c2ab59f97434590e2d55af0e0879
Carlos Arturo Medina García <carlos@carlosmac.local>
2020-04-08 17:42:48 -0500

    Initial commit

---
+++ BUC.py
@@ -0,0 +1,86 @@
+import csv
+
+
+def partition(dataset_list, dimension_index, dimension_attributes, lst):
+    item = []
+    for attribute in dimension_attributes:
+        item.append(sum(list(map(int, [(i[4] if i[dimension_index] == attribute else 0) for i in dataset_list]))))
+    lst[dimension_index] = item
+
+
+def new_input(dataset_list, d, dimension_attribute_name):
+    res = [i for i in dataset_list if i[d] == dimension_attribute_name]
+    return res
+
+
+def validate_input():
+    try:
+        user = int(input("Input minimum_support: "))
+    except ValueError:
+        print("Bad Input")
+    return user
+
+
+records = []
+headers = []
+with open('Product_Sales_Data_Set.csv', newline='', encoding='utf-8-sig') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        records.append(row)
+
+headers = records[0]
+records = records[1:]
+
+items = ['Computer', 'Camera', 'Phone', 'Printer']
+locations = ['Toronto', 'Vancouver', 'New York', 'Chicago']
+years = ['2017', '2018']
+dimensions = [items, locations, years]
+
+dataCount = [[None], [None], [None]]
+
+f = open('Iceberg-Cube-Results.txt', 'w')
+summary = []
+last = ["","",""]
+
+def BUC(input, d):
+    x = input
+    while d < 3:
+        C = len(dimensions[d])
+        if (d == 1 and len(input) == 128) or (d == 2 and len(input) == 32):
+            break
+        partition(input, d, dimensions[d], dataCount)
+        tc = dataCount
+        for i in range(C):
+            c = tc[d][i]
+            if c >= min_sup:
+                f.write('\t\t' * 2 * d)
+                f.write(dimensions[d][i] + '\t' + str(c) + '\n')
+                if d == 0:
+                    key = dimensions[d][i]
+                    last[d] = dimensions[d][i]
+                elif d == 1:
+                    key = last[0] + dimensions[d][i]
+                    last[d] = dimensions[d][i]
+                elif d == 2:
+                    key = last[0] + last[1] + dimensions[d][i]
+                    last[d] = dimensions[d][i]
+                summary.append((key,str(c)))
+                BUC(new_input(input, d, dimensions[d][i]), d + 1)
+            else:
+                f.write('\t\t' * 2 * d)
+                f.write(dimensions[d][i] + '\t' + '*' + '\n')
+                BUC(new_input(input, d, dimensions[d][i]), d + 1)
+        d = d + 1
+
+
+min_sup = validate_input()
+BUC(records, 0)
+print(summary)
+
+#dict_sum = {}
+#for tup in summary:
+#    if tup[0] in dict_sum:
+#        dict_sum[tup[0]] = int(dict_sum[tup[0]]) + int(tup[1])
+#    else:
+#        dict_sum[tup[0]] = int(tup[1])
+#print(dict_sum)
---
+++ README.md
@@ -0,0 +1,26 @@
+# BUC-Iceberg-Cube
+An implementation of the Bottom-Up Computation (BUC) algorithm for computing Iceberg cubes.
+
+## Execute:
+`python3 BUC.py`
+
+## More Info:
+1. The program needs to be executed in the same directory that has the dataset
+named `Product_Sales_Data_Set.csv`.
+
+2. The generated output file is called `Iceberg-Cube-Results.txt`
+
+3. Absence of data points is represented by *
+
+4. Given example has `minimum_support = 11`
+
+
+## 5.11 Answers :
+    a) La implementación de BUC se realizó en Python, ejecutándose sobre un CSV encontrado en Internet sobre venta de productos; el programa realiza el cálculo de la agregación (suma) en unidades de venta sobre los distintos valores encontrados por dimensión. En este caso se acotó a tres dimensiones: items, location y years.
+    El programa pide como parámetro de entrada el soporte mínimo, lee los datos del csv e imprime el resumen del cubo de datos mediante un arreglo con tuplas de la forma (dim i: agregación); imprimiendo sólo aquellos cuya agregación es mayor igual al soporte mínimo definido por el usuario.
+    El programa muestra un buen desempeño, ya que el conjunto de datos no es grande; y al al aumentar el soporte mínimo la cantidad podada es bastante significativa y ayuda a evitar obtener datos no representativos.
+
+    b) i) El tamaño del cuboide aumenta exponencialmente con el número de dimensiones; por lo que se requiere de    mayor cantidad de memoria así como de tiempo necesario para calcular el cuboide.
+        ii) Un cubo iceberg, al eliminar celdas con agregaciones pequeñas y reducir por tanto el costo computacional. Los beneficios pueden ser mayores si el dataset es disperso, más no cuando tiene sesgo. Esto se debe a que existen poca posibilidad que las celdas se colapsen en la misma celda agregada, excepto en cuboides con pocas dimensiones. Por tanto, muchas celdas pueden tener valores menores al soporte mínimo y serán omitidas.
+        iii) Considerar una base de datos de 100 dimensiones. Sea a,ij el jésimo valor de la dimensión i. Asumiendo 10 celdas en el cuboide base, todas agregadas a la celda (a1,1, a2,1,...,a99,1, *). Siendo el soporte mínimo 10, 2^99-1 celdas agregadas de esta celda satisfacen el límite. En este caso no habría algún beneficio por podamiento.
+    c) BUC puede modificarse para generar un shell cube de alguna combinación de cantidad de dimensiones específica, ya que parte del apex hacia abajo, y mientras lo hace se puede validar que corresponda a los valores deseados y deteniendolo cuando alcance el máximo número de dimensiones.
